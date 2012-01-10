c0d0db44e3b8e6e91f1cfbee78814ca4426e8fe0
Alexandra Yamaui <juanlagrange@Juans-iMac.local>
2012-01-10 17:45:08 -0430

    creo que bien hasta aqui

---
+++ spec/factories.rb
@@ -0,0 +1,10 @@
+￼# By using the symbol ':user', we get Factory Girl to simulate the User model.
+Factory.define :user do |user|
+  user.name
+  user.email
+  user.password
+  user.password_confirmation "foobar"
+  "Michael Hartl"
+  "mhartl@example.com"
+  "foobar"
+end
