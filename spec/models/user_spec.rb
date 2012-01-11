f2a8210cd687f3eb0d147fc090a5d4a21cc5604c
Alexandra <alexandra.yamaui@gmail.com>
2012-01-11 17:27:42 -0430

    tiene error

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -126,17 +126,17 @@ describe User do
       it "should return nil on email/password mismatch" do
 
         wrong_password_user = User.authenticate(@attr[:email], "wrongpass")
-	wrong password_user.should be_nil
+	      wrong_password_user.should be_nil
      end 
 
      it "should return nil for an email address with no user" do
-	nonexistent_user = User.authenticate("bar@foo.com", @attr[:password])
-	nonexistent_user.should be_nil
+	     nonexistent_user = User.authenticate("bar@foo.com", @attr[:password])
+	     nonexistent_user.should be_nil
      end
 
      it "should return the user on email/password match" do
-	matching_user = User.authenticate(@attr[:email], @attr[:password])
-	matching_user.should == @user
+	     matching_user = User.authenticate(@attr[:email], @attr[:password])
+	     matching_user.should == @user
      end
    end
   end
