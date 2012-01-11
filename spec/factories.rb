ddda9526285d85b96797c840dce32954c44bc268
Alexandra <alexandra.yamaui@gmail.com>
2012-01-11 10:57:11 -0430

    Punto de control (todo bien)

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,10 +1,7 @@
-￼# By using the symbol ':user', we get Factory Girl to simulate the User model.
+#By using the symbol ':user', we get Factory Girl to simulate the User model.
 Factory.define :user do |user|
-  user.name
-  user.email
-  user.password
-  user.password_confirmation "foobar"
-  "Michael Hartl"
-  "mhartl@example.com"
-  "foobar"
+  user.name "Michael Hartl"
+  user.email "mhartl@example.com"
+  user.password "foobar"
+  user.password_confirmation "foobar"  
 end
