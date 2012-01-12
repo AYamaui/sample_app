61628ba97034bc00e71bb3f4f596bb344a01fffe
Alexandra <alexandra.yamaui@gmail.com>
2012-01-12 02:22:39 -0430

    Hasta aqui todo bien (Terminado cap 8)

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -46,7 +46,7 @@ describe UsersController do
     end 
   end
   
-  describe "POST 'create" do
+  describe "POST 'create'" do
     
     describe "failure" do
       
@@ -54,11 +54,11 @@ describe UsersController do
         @attr = { :name => "", :email => "", :password => "", :assword_confirmation => ""}
       end
       
-      it "should not create a user" do
-        lambda do
-          post :create, :user => @attr
-          end.response.should_not change(User, :count)
-      end
+     it "should not create a user" do
+       lambda do
+         post :create, :user => @attr
+       end.should_not change(User, :count)
+     end
       
       it "should have the right title" do
         post :create, :user => @attr
