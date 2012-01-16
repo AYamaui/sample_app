54ea76306ec59bc480a16fdd3f7e8be741e632ed
Alexandra Yamaui <alexandra.yamaui@gmail.com>
2012-01-16 17:26:46 -0430

    Experimentando con los apis

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,6 +24,27 @@ class UsersController < ApplicationController
     if @user.save
       sign_in @user
       flash[:success] = "Welcome to the Sample App!"
+      
+      #Parte de twitter------------
+      
+      client = TwitterOAuth::Client.new(
+          :consumer_key => '8HMEWgi9jCjSS8DzWyX8Q',
+          :consumer_secret => 'N96T1OPRx5vQgCIzoKVMKntXcALkckNmt4mCwaKioGA'
+      )
+
+      request_token = client.request_token(:oauth_callback => oauth_confirm_url)
+      #@access_token = @request_token.get_access_token(:oauth_verifier =>
+      #params[:oauth_verifier])
+      binding.pry
+      #:oauth_callback required for web apps, since oauth gem by default force PIN-based flow
+      #( see http://groups.google.com/group/twitter-development-talk/browse_thread/thread/472500cfe9e7cdb9/848f834227d3e64d )
+
+      #request_token.authorize_url => http://twitter.com/oauth/authorize?oauth_token=TOKEN
+      #Link this url to in your view file, so that user will be redirected to the Twitter authentication page.
+      
+      #Termina parte de twitter--------
+      
+      
       redirect_to @user
     else
       @title = "Sign up"
