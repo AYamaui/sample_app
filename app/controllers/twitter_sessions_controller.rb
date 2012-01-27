a72484c06fb438f6da081bf8ff41629989de9892
Alexandra <alexandra.yamaui@gmail.com>
2012-01-27 10:52:00 -0430

    t: quitando un binding.pry

--- app/controllers/twitter_sessions_controller.rb
+++ app/controllers/twitter_sessions_controller.rb
@@ -21,7 +21,6 @@ class TwitterSessionsController < ApplicationController
   end
 
   def callback   
-    binding.pry
     request_token = OAuth::RequestToken.new(oauth_consumer, session['request_token'], session['request_secret'])
     access_token = request_token.get_access_token(:oauth_verifier => params[:oauth_verifier])
     
