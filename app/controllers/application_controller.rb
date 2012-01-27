d94dbc0edc57c824039f9847aa5fd8c5d9b5e2db
Alexandra <alexandra.yamaui@gmail.com>
2012-01-27 16:38:09 -0430

    Casi listo lo de twitter (sin binding.pry)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -30,7 +30,7 @@ class ApplicationController < ActionController::Base
         config.oauth_token_secret = session['access_secret']
       end
     else
-      binding.pry
+      #binding.pry
       Twitter.configure do |config|
         config.consumer_key = '8HMEWgi9jCjSS8DzWyX8Q'
         config.consumer_secret = 'N96T1OPRx5vQgCIzoKVMKntXcALkckNmt4mCwaKioGA'
